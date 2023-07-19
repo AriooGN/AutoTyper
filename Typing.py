176c4f3c229c5af7f5aac5ddd13b7dc852434fb3
arioo_ <arianfooladray@gmail.com>
2023-07-18 23:30:29 -0400

    close to final version inshallah
    Added human typing mode, and a wpm

--- Typing.py
+++ Typing.py
@@ -1,92 +1,75 @@
-#Code Related
 import pyautogui
 import time
 import random
-
-#GUI Related
 from tkinter import *
 import customtkinter
 
-
-#Initilize Instance Variables
-filename = "text.txt"
 printToggle=True
+typing_speed = 0.1  # initial typing speed in seconds per character
+words_per_minute = 60  # initial words per minute
 
-#Varying Print Time Range
-def randomNumber():
-    return random.uniform(0.1712, 0.24)
+def randomNumber(x, y, type):
+    if type == 0:
+        return random.uniform(x, y)
+    elif type == 1:
+        return random.randint(x, y)
 
-#Pull Text From File and Returns a textInput
-def readFile():
-        try:
-            with open(filename, 'r') as file:
-                textInput = file.read().strip()
-                if textInput=="":
-                    print("\nThere is no contents in the file\n")
-                    return None
-                else:
-                    return textInput     
-        except FileNotFoundError:
-            print("File not found.")
-            exit()
-    
-
-#Function that Pulls text and Begins Typing
 def getTextthenPrint():
     global printToggle
-
-    #CheckFile Contents First if none then use Text Box
-    if (readFile()!=None):
-        textInput=readFile()
-        print("\n\nThis is the content of the file:\n" + textInput)
-    else:
-        textInput= my_entry.get(1.0, END)
-        print("\n\nThis is the content of the Entry:\n" + textInput)
-        
+    global typing_speed
+    textInput= my_entry.get(1.0, END)
+    print("\n\nThis is the content of the Entry:\n" + textInput)
 
     arrayTextInput = list(textInput)
 
-
-    #print("Are you sure you want to type this?")
-    #yesNoInput = input("y/n: ")
     yesNoInput ="y"
-
-    answer = True if yesNoInput.lower() == "y" else False
+    answer = yesNoInput.lower() == "y"
 
     intCounter = 0
+    intCoolDownTimer = 0
     if answer:
         time.sleep(5)
-        if( printToggle== True):
+        longPause = randomNumber(40, 50, 1)
+        if printToggle:
             while len(arrayTextInput) > intCounter:
-                if(arrayTextInput[intCounter] == " "):
-                    time.sleep(randomNumber() * 0.5)
-                pyautogui.write(arrayTextInput[intCounter])
-                #time.sleep(randomNumber() * 0.0025)
+                if arrayTextInput[intCounter] == " " and human_mode.get():
+                    x = randomNumber(typing_speed/2, typing_speed*2, 0)
+                    time.sleep(x)
+                    print(x)
+                pyautogui.write(arrayTextInput[intCounter], interval=typing_speed)
                 intCounter += 1
+                intCoolDownTimer += 1
+                if intCoolDownTimer == longPause and human_mode.get():
+                    y = time.sleep(randomNumber(8, 15, 0))
+                    print(y)
+                    intCoolDownTimer = 0
+                    longPause = randomNumber(40,50,1)
+                if arrayTextInput[intCounter] == " " and 2 == randomNumber(1, 5, 1) and human_mode.get():
+                    time.sleep(randomNumber(2, 5, 0))
         else:
              printToggle= True
 
 def fakeExit():
     global printToggle
     printToggle=False
-    return   
-
-#GUI Starting
-
-customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
-
-root = customtkinter.CTk()
-
-root.title('AutoTyper')
-
-#Change Icon Image
-#root.iconbitmap('c:/gui/codemy.ico')
-root.geometry("620x470")
+    return
 
 def Clear():
-	# Clear The text
-	my_entry.delete(1.0, END)
+    my_entry.delete(1.0, END)
+
+def adjust_typing_speed(wpm):
+    global typing_speed
+    words_per_minute = int(wpm)
+    typing_speed = 60 / (words_per_minute * 5)  # based on the standard definition of wpm
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.title('AutoTyper')
+root.geometry("620x470")
+
+human_mode = BooleanVar(value=True)  # initial human typing mode, created after root window
 
 my_labelframe = customtkinter.CTkFrame(root, corner_radius=10)
 my_labelframe.pack(pady=10)
@@ -100,11 +83,21 @@ my_entry.grid(row=0, column=0, pady=10)
 Start_button = customtkinter.CTkButton(my_labelframe, text="Start Typing", command=getTextthenPrint)
 Start_button.grid(row=1, column=0, padx=1, pady=10)
 
-
 Stop_button = customtkinter.CTkButton(my_labelframe, text="Stop Typing", command=fakeExit)
 Stop_button.grid(row=1, column=1, padx=1, pady=10)
 
 Clear_button = customtkinter.CTkButton(my_labelframe, text="Clear", command=Clear)
 Clear_button.grid(row=1, column=2, padx=1, pady=10)
+
+# Typing speed slider
+speed_label = Label(my_labelframe, text="Typing Speed (WPM):")
+speed_label.grid(row=2, column=0)
+speed_scale = Scale(my_labelframe, from_=10, to=120, resolution=5, orient=HORIZONTAL, command=adjust_typing_speed)
+speed_scale.set(words_per_minute)
+speed_scale.grid(row=2, column=1)
+
+# Human typing mode toggle
+human_mode_toggle = Checkbutton(my_labelframe, text="Human Typing Mode", variable=human_mode)
+human_mode_toggle.grid(row=3, column=0)
 
 root.mainloop()
