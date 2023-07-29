d4104474a607e61daceace613a211469737e3df3
arioo_ <arianfooladray@gmail.com>
2023-07-29 15:39:39 -0400

    Final Push

--- Typing.py
+++ Typing.py
@@ -1,70 +1,86 @@
-import pyautogui
+# Cleaned up code with more descriptive constant names
+
 import time
 import random
-from tkinter import *
+import threading
 import customtkinter
+from tkinter import *
 
-printToggle=True
-typing_speed = 0.1  # initial typing speed in seconds per character
-words_per_minute = 60  # initial words per minute
+# Constants
+INITIAL_TYPING_SPEED = 0.1  # The initial typing speed specified in seconds per character.
+INITIAL_WORDS_PER_MINUTE = 60  # The initial typing speed specified in words per minute.
+PRINT_TOGGLE = True  # Boolean flag to control the printing status.
 
-def randomNumber(x, y, type):
-    if type == 0:
-        return random.uniform(x, y)
-    elif type == 1:
-        return random.randint(x, y)
+# Parameters for random pauses between a range of characters.
+CHARACTER_PAUSE_MIN = 60  # The minimum limit for character pause.
+CHARACTER_PAUSE_MAX = 150  # The maximum limit for character pause.
+REST_PERIOD_MIN = 3  # The minimum limit for rest period.
+REST_PERIOD_MAX = 10  # The maximum limit for rest period.
 
-def getTextthenPrint():
-    global printToggle
+# Parameters to control the probability of a pause occurring between words.
+WORD_PAUSE_MIN = 1  # The minimum limit for word pause.
+WORD_PAUSE_MAX = 2  # The maximum limit for word pause.
+
+# Parameters to determine the chance of a pause occurring between words.
+NUMBER_FOR_CHANCE = 2  # A random number compared to for a pause chance.
+CHANCE_WORD_PAUSE_MIN = 1  # The minimum limit for the chance of word pause.
+CHANCE_WORD_PAUSE_MAX = 5  # The maximum limit for the chance of word pause.
+
+# Functions
+
+def generate_random_number(min_value, max_value, number_type):
+    if number_type == 0:
+        return random.uniform(min_value, max_value)
+    elif number_type == 1:
+        return random.randint(min_value, max_value)
+
+def print_text():
+    global PRINT_TOGGLE
     global typing_speed
-    textInput= my_entry.get(1.0, END)
-    print("\n\nThis is the content of the Entry:\n" + textInput)
+    text_input = my_entry.get(1.0, END)
+    text_input_list = list(text_input)
+    counter = 0
+    cool_down_timer = 0
+    time.sleep(2)
+    character_pause = generate_random_number(CHARACTER_PAUSE_MIN, CHARACTER_PAUSE_MAX, 1)
 
-    arrayTextInput = list(textInput)
+    for char in text_input_list:
+        if not PRINT_TOGGLE:
+            PRINT_TOGGLE = True
+            return
+        if char == " " and human_mode.get():
+            pause_time = generate_random_number(typing_speed/2, typing_speed*2, 0)
+            time.sleep(pause_time)
+            print(pause_time)
+        pyautogui.write(char, interval=typing_speed)
+        counter += 1
+        cool_down_timer += 1
+        if cool_down_timer == character_pause and human_mode.get():
+            y = time.sleep(generate_random_number(REST_PERIOD_MIN, REST_PERIOD_MAX, 0))
+            print(y)
+            cool_down_timer = 0
+            character_pause = generate_random_number(CHARACTER_PAUSE_MIN, CHARACTER_PAUSE_MAX, 1)
+        if char == " " and NUMBER_FOR_CHANCE == generate_random_number(CHANCE_WORD_PAUSE_MIN, CHANCE_WORD_PAUSE_MAX, 1) and human_mode.get():
+            time.sleep(generate_random_number(WORD_PAUSE_MIN, WORD_PAUSE_MAX, 0))
 
-    yesNoInput ="y"
-    answer = yesNoInput.lower() == "y"
-
-    intCounter = 0
-    intCoolDownTimer = 0
-    if answer:
-        time.sleep(5)
-        longPause = randomNumber(40, 50, 1)
-        if printToggle:
-            while len(arrayTextInput) > intCounter:
-                if arrayTextInput[intCounter] == " " and human_mode.get():
-                    x = randomNumber(typing_speed/2, typing_speed*2, 0)
-                    time.sleep(x)
-                    print(x)
-                pyautogui.write(arrayTextInput[intCounter], interval=typing_speed)
-                intCounter += 1
-                intCoolDownTimer += 1
-                if intCoolDownTimer == longPause and human_mode.get():
-                    y = time.sleep(randomNumber(8, 15, 0))
-                    print(y)
-                    intCoolDownTimer = 0
-                    longPause = randomNumber(40,50,1)
-                if arrayTextInput[intCounter] == " " and 2 == randomNumber(1, 5, 1) and human_mode.get():
-                    time.sleep(randomNumber(2, 5, 0))
-        else:
-             printToggle= True
-
-def fakeExit():
-    global printToggle
-    printToggle=False
+def stop_typing():
+    global PRINT_TOGGLE
+    PRINT_TOGGLE = False
     return
 
-def Clear():
+def clear_text():
     my_entry.delete(1.0, END)
 
 def adjust_typing_speed(wpm):
     global typing_speed
     words_per_minute = int(wpm)
     typing_speed = 60 / (words_per_minute * 5)  # based on the standard definition of wpm
-    
-def Update_slider(wpm):
+
+def update_slider(wpm):
     adjust_typing_speed(wpm)
-    WPM_label.configure(text =str(wpm) +" (WPM)")
+    wpm_label.configure(text =str(wpm) +" (WPM)")
+
+# Main Code
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("green")
@@ -84,29 +100,29 @@ text_frame.pack(pady=10)
 my_entry = Text(text_frame, height=20, width=67, wrap=WORD, bd=0, bg="#292929", fg="silver")
 my_entry.grid(row=0, column=0, pady=10)
 
-Start_button = customtkinter.CTkButton(my_labelframe, text="Start Typing", command=getTextthenPrint)
-Start_button.grid(row=1, column=0, padx=1, pady=10)
+start_button = customtkinter.CTkButton(my_labelframe, text="Start Typing", command=lambda: threading.Thread(target=print_text).start())
+start_button.grid(row=1, column=0, padx=1, pady=10)
 
-Stop_button = customtkinter.CTkButton(my_labelframe, text="Stop Typing", command=fakeExit)
-Stop_button.grid(row=1, column=1, padx=1, pady=10)
+stop_button = customtkinter.CTkButton(my_labelframe, text="Stop Typing", command=stop_typing)
+stop_button.grid(row=1, column=1, padx=1, pady=10)
 
-Clear_button = customtkinter.CTkButton(my_labelframe, text="Clear", command=Clear)
-Clear_button.grid(row=1, column=2, padx=1, pady=10)
+clear_button = customtkinter.CTkButton(my_labelframe, text="Clear", command=clear_text)
+clear_button.grid(row=1, column=2, padx=1, pady=10)
 
 # Typing speed slider
-speed_label = Label(my_labelframe, text="Typing Speed (WPM):")
+speed_label = customtkinter.CTkLabel(my_labelframe, text="Typing Speed (WPM):")
 speed_label.grid(row=2, column=0)
-speed_scale = customtkinter.CTkSlider(my_labelframe, number_of_steps=22,from_=10, to=120, orientation=HORIZONTAL, command=Update_slider)
-speed_scale.set(words_per_minute)
+speed_scale = customtkinter.CTkSlider(my_labelframe, number_of_steps=22, from_=10, to=120, orientation=HORIZONTAL, command=update_slider)
+speed_scale.set(INITIAL_WORDS_PER_MINUTE)
 speed_scale.grid(row=2, column=1)
 
 # Displayed Typing speed
-WPM_label = Label(my_labelframe, text= str(words_per_minute) +" (WPM)")
-WPM_label.grid(row=2, column=2)
-
+wpm_label = customtkinter.CTkLabel(my_labelframe, text= str(INITIAL_WORDS_PER_MINUTE) +" (WPM)")
+wpm_label.grid(row=2, column=2)
 
 # Human typing mode toggle
-human_mode_toggle = Checkbutton(my_labelframe, text="Human Typing Mode", variable=human_mode)
-human_mode_toggle.grid(row=3, column=0)
+human_mode_toggle = customtkinter.CTkCheckBox(my_labelframe, text="Human Typing Mode", variable=human_mode)
+human_mode_toggle.grid(row = 3, column = 0)
 
 root.mainloop()
+
